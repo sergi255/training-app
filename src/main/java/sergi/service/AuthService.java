9cfa54dd1b38d2f4a01a0315e714ed9fd9c362d1
sergi255 <sergiuszskalski@gmail.com>
2025-01-19 12:43:46 +0100

    feat: added JWT authentication

---
+++ src/main/java/sergi/service/AuthService.java
@@ -0,0 +1,63 @@
+package sergi.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import sergi.dto.AuthRequest;
+import sergi.dto.AuthResponse;
+import sergi.dto.RegisterRequest;
+import sergi.entity.Role;
+import sergi.entity.User;
+import sergi.repository.UserRepository;
+import sergi.security.JwtService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    @Transactional(rollbackFor = Exception.class)
+    public AuthResponse register(RegisterRequest request) {
+        if (userRepository.findByUsername(request.getUsername()).isPresent()) {
+            throw new RuntimeException("Username already exists");
+        }
+        if (userRepository.findByEmail(request.getEmail()).isPresent()) {
+            throw new RuntimeException("Email already exists");
+        }
+
+        User user = User.builder()
+                .username(request.getUsername())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+
+        userRepository.save(user);
+        return AuthResponse.builder()
+                .token("Registration successful")
+                .build();
+    }
+
+    public AuthResponse login(AuthRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getUsername(),
+                        request.getPassword()
+                )
+        );
+
+        var user = userRepository.findByUsername(request.getUsername())
+                .orElseThrow();
+        var token = jwtService.generateToken((UserDetails) user);
+        return AuthResponse.builder()
+                .token(token)
+                .build();
+    }
+}

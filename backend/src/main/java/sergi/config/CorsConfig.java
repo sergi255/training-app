0be8a0ad10fe3614e5a4cb799a2b1727aed6d815
sergi255 <sergiuszskalski@gmail.com>
2025-01-23 22:02:32 +0100

    fix: update CORS configuration to allow requests from 127.0.0.1:5173

--- backend/src/main/java/sergi/config/CorsConfig.java
+++ backend/src/main/java/sergi/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("http://localhost:5173", "127.0.0.1:5173")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH", "HEAD")
                         .allowedHeaders("Authorization", "Content-Type", "X-Requested-With", "accept", "Origin", 
                                       "Access-Control-Request-Method", "Access-Control-Request-Headers")
